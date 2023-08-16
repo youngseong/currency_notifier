fa24bbc29026c39a84b89929de58ab8410b4a3b4
Youngseong <youngseong0601@gmail.com>
2023-08-16 13:26:33 +0200

    init main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# TODO's
+# 0. parse config to set api keys and methods
+# 1. query currency exchange rate
+# 2. check if the rate triggers a condition
+# 3. if so, alert to the user
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
