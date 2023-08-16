b3d2e3c85320e008a2e79804e50bb7fad36a244b
Youngseong <youngseong0601@gmail.com>
2023-08-16 13:26:33 +0200

    update main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from config_loader.config_loader import load_config
 from currency_checkers.geoapi import check_currency_rate
+from triggers.threshold_trigger import threshold_trigger
 from pathlib import Path
 
 
@@ -7,11 +8,10 @@ def main():
     config_dir = Path(__file__).parent / 'config'
     config = load_config(config_dir / 'base.json', config_dir / 'young.json')
 
-    currency_rate_for_amount = check_currency_rate(**config)
+    expected_amount = check_currency_rate(**config['currency'])
+    good = threshold_trigger(expected_amount, **config['trigger'])
 
-    # TODO's
-    # 2. check if the rate triggers a condition
-    # 3. if so, alert to the user
+    # TODO: alert currency rate and suggestion (i.e., exchange or not) to the user
 
 
 if __name__ == '__main__':
