875b01f94e08c48543001bfba66f72e5a4602486
Youngseong <youngseong0601@gmail.com>
2023-08-16 13:26:33 +0200

    finish first impl of main

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
+import asyncio
+from datetime import datetime
+from pathlib import Path
+
 from config_loader.config_loader import load_config
 from currency_checkers.geoapi import check_currency_rate
 from triggers.threshold_trigger import threshold_trigger
-from pathlib import Path
+from notifiers.telegram_notifier import TelegramNotifier
 
 
-def main():
+def generate_message(amount:float, good:bool, trigger:dict):
+    lines = [
+        datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        f'Suggestion: {str(good)}',
+        f'Expected Target Amount: {amount}',
+        f'Trigger: {trigger}'
+    ]
+    return '\n'.join(lines)
+
+
+async def main():
     config_dir = Path(__file__).parent / 'config'
     config = load_config(config_dir / 'base.json', config_dir / 'young.json')
 
     expected_amount = check_currency_rate(**config['currency'])
     good = threshold_trigger(expected_amount, **config['trigger'])
 
-    # TODO: alert currency rate and suggestion (i.e., exchange or not) to the user
+    msg = generate_message(expected_amount, good, config['trigger'])
+
+    notifier = TelegramNotifier(**config['notification'])
+    await notifier.notify(msg)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
