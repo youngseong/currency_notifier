944537c226163f90c5cb124903ebe24fc9f45c67
Youngseong <youngseong0601@gmail.com>
2023-08-16 13:26:33 +0200

    add threshold trigger

---
+++ triggers/threshold_trigger.py
@@ -0,0 +1,5 @@
+def threshold_trigger(amount: float, threshold: float, comparator: str):
+    assert comparator in set(['>', '<', 'gt', 'lt'])
+
+    def cmp(a, b): return a > b if comparator == '>' or comparator == 'gt' else a < b
+    return cmp(amount, threshold)
