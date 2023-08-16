ff719622e1483f36c7d160265c9bccb7d2be4e3f
Youngseong <youngseong0601@gmail.com>
2023-08-16 13:26:33 +0200

    fill main.py a bit

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-# TODO's
-# 0. parse config to set api keys and methods
-# 1. query currency exchange rate
-# 2. check if the rate triggers a condition
-# 3. if so, alert to the user
+from config_loader.config_loader import load_config
+from currency_checkers.geoapi import check_currency_rate
+from pathlib import Path
+
 
 def main():
-    pass
+    config_dir = Path(__file__).parent / 'config'
+    config = load_config(config_dir / 'base.json', config_dir / 'young.json')
+
+    currency_rate_for_amount = check_currency_rate(**config)
+
+    # TODO's
+    # 2. check if the rate triggers a condition
+    # 3. if so, alert to the user
+
 
 if __name__ == '__main__':
     main()
