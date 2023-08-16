2cc628d15767a372cabb99feb3095c88c95500e9
Youngseong <youngseong0601@gmail.com>
2023-08-16 13:26:33 +0200

    impl telegram notifier

---
+++ notifiers/telegram_notifier.py
@@ -0,0 +1,48 @@
+import asyncio
+import telegram
+from typing import List
+
+
+class TelegramNotifier(object):
+    def __init__(self, token: str, chat_ids: List[int]):
+        self._bot = telegram.Bot(token)
+
+        self._chat_ids = chat_ids
+
+    @property
+    def chat_ids(self):
+        return self._chat_ids
+
+    async def update_chat_ids(self):
+        self._chat_ids = await self.__list_chat_ids()
+
+    async def __list_chat_ids(self):
+        updates = await self._bot.get_updates()
+        chat_ids = [u.effective_user.id for u in updates if u.effective_user]
+        return chat_ids
+
+    async def notify(self, text: str):
+        await asyncio.gather(*[self.send_message(id, text) for id in self._chat_ids])
+
+    async def send_message(self, chat_id: int, text: str):
+        await self._bot.send_message(text=text, chat_id=chat_id)
+
+
+async def main(token: str):
+    notifier = TelegramNotifier(token, [])
+
+    await notifier.update_chat_ids()
+    print(notifier.chat_ids)
+
+    await notifier.send_message(notifier.chat_ids[0], 'Hello world!')
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('token', type=str)
+
+    args = arg_parser.parse_args()
+
+    asyncio.run(main(args.token))
