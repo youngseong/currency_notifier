c1f4d318ef2e116d8b984c4494c00ebfc815abcd
Youngseong <youngseong0601@gmail.com>
2023-08-20 15:47:54 +0200

    set default values for currency args

--- currency_checkers/geoapi.py
+++ currency_checkers/geoapi.py
@@ -2,7 +2,7 @@ import requests
 
 
 # TODO: generalize the function
-def check_currency_rate(api_key: str, source_currency: str, target_currency: str, **kwargs):
+def check_currency_rate(api_key: str, source_currency: str = 'KRW', target_currency: str = 'EUR', **kwargs):
     target_currency = kwargs.get('target_currency', 'EUR')
 
     parameters = {
