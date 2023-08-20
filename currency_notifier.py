41a02a185e5af363d847863be19922b7b6ced0de
Youngseong <youngseong0601@gmail.com>
2023-08-20 15:56:28 +0200

    update noti msg

--- currency_notifier.py
+++ currency_notifier.py
@@ -8,16 +8,17 @@ from triggers.threshold_trigger import threshold_trigger
 from notifiers.telegram_notifier import TelegramNotifier
 
 
-def generate_message(source_amount:float,
-                     target_amount:float,
+def generate_message(dst_amount:float,
                      good:bool,
-                     trigger:dict):
+                     currency_setting:dict,
+                     trigger_setting:dict):
+    src_curr, dst_curr, src_amount = currency_setting['from'], currency_setting['to'], currency_setting['amount']
     lines = [
         datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
         'Suggestion: ' + ('Exchange' if good else 'Wait'),
-        f'Expected Target Amount: {target_amount}',
-        f'Source Amount: {source_amount}',
-        f'Trigger: {trigger}'
+        f'From ({src_curr}): {src_amount}',
+        f'To ({dst_curr}): {dst_amount}',
+        f'Trigger: {trigger_setting}'
     ]
     return '\n'.join(lines)
 
@@ -29,8 +30,7 @@ async def main():
     expected_amount = check_currency_rate(**config['currency'])
     good = threshold_trigger(expected_amount, **config['trigger'])
 
-    source_amount = config['currency'].get('amount', 1)
-    msg = generate_message(source_amount, expected_amount, good, config['trigger'])
+    msg = generate_message(expected_amount, good, config['currency'], config['trigger'])
 
     notifier = TelegramNotifier(**config['notification'])
     await notifier.notify_all(msg)
